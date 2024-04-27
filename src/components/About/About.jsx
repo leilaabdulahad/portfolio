4f9b0579497cc4c08f02cdbc6f66ecdf6f9711d6
Leila <leilaabdulahad@hotmail.com>
2024-04-27 15:21:32 +0200

    updated about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ function About() {
     <div id='about' className={styles.about}>
       <h2 className={styles.heading}>About Me</h2>
       <p className={styles.description}>
-        One of the most thrilling aspects of software development for me is the power to transform abstract concepts into concrete creations. I relish the journey of nurturing an idea into a tangible product. Continuously seeking to enhance my skills and broaden my understanding, I thrive on challenges and pride myself on being a fast learner and problem solver.
+        One of the most thrilling aspects of software development for me is the power to transform abstract concepts into concrete creations. I'm continuously seeking to enhance my skills and broaden my understanding, I thrive on challenges and pride myself on being a fast learner and a problem solver.
       </p>
     </div>
     </Tilty>
