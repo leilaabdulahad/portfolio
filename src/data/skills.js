ba36f1c2ac421f6dd0a3f012419086af35ea821c
Leila <leilaabdulahad@hotmail.com>
2024-08-09 14:00:02 +0200

    moved the skills data to a seperate file

---
+++ src/data/skills.js
@@ -0,0 +1,54 @@
+import tailwindImage from '../../assets/skills/tailwind.png'
+import reactImage from '../../assets/skills/react.png'
+import nodeImage from '../../assets/skills/nodejs.png'
+import expressImage from '../../assets/skills/express.png'
+import mongoImage from '../../assets/skills/mongodb.png'
+import jsImage from '../../assets/skills/javascript.png'
+import reduxImage from '../../assets/skills/redux.png'
+import nextjsImage from '../../assets/skills/nextjs.png'
+import vuejsImage from '../../assets/skills/vue.png'
+import convex from '../../assets/skills/convex.png'
+
+export const skills = [
+    {
+      "title": "JavaScript",
+      "imageSrc": jsImage
+    },
+    {
+      "title": "React",
+      "imageSrc": reactImage
+    },
+    {
+      "title": "Next.js",
+      "imageSrc": nextjsImage
+    },
+    {
+      "title": "Vue.js",
+      "imageSrc": vuejsImage
+    },
+    {
+      "title": "Node.js",
+      "imageSrc": nodeImage
+    },
+    {
+      "title": "MongoDB",
+      "imageSrc": mongoImage
+    },
+    {
+      "title": "Express",
+      "imageSrc": expressImage
+    },
+    {
+      "title": "Convex",
+      "imageSrc": convex
+    },
+    {
+      "title": "Redux",
+      "imageSrc": reduxImage
+    },
+    {
+      "title": "Tailwind",
+      "imageSrc": tailwindImage
+    },
+  ]
+  
