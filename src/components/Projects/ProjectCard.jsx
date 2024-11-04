9075130133a721c1219998c0518f616eda60643a
Leila <leilaabdulahad@hotmail.com>
2024-11-04 21:11:52 +0100

    adds another project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -6,42 +6,50 @@ export const ProjectCard = ({
   project: { title, imageSrc, description, demo, source, youtube },
 }) => {
   const [isReadMore, setIsReadMore] = useState(false);
-  const shortDescription = description.split(" ").slice(0, 28).join(" ");
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${isReadMore ? styles.expanded : ''}`}>
       <h3 className={styles.title}>{title}</h3>
       <img
         src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
         className={styles.image}
-        />
-      <p className={styles.description}>
-        {isReadMore ? description : `${shortDescription}...`}
-        <button className={styles.readMoreButton} onClick={() => setIsReadMore(!isReadMore)}>
-          {isReadMore ? "show less" : "read more"}
-        </button>
-      </p>
-      <div className={styles.links}>
-          <button className={styles.githubButton}>
-            <a href={source} className={styles.link}>
-              GitHub
-            </a>
+      />
+      <div className={styles.contentWrapper}>
+        <p className={`${styles.description} ${!isReadMore ? styles.collapsed : ''}`}>
+          {description}
+        </p>
+        
+        <div className={styles.buttonContainer}>
+          <button 
+            className={styles.readMoreButton} 
+            onClick={() => setIsReadMore(!isReadMore)}
+          >
+            {isReadMore ? "show less" : "read more"}
           </button>
-        {demo && (
-          <button className={styles.githubButton}>
-          <a href={demo} className={styles.link}>
-          Live demo
-        </a>
-        </button>
-          )}
-          {youtube && (
+          
+          <div className={styles.links}>
             <button className={styles.githubButton}>
-            <a href={youtube} className={styles.link}>
-              YouTube
-            </a>
+              <a href={source} className={styles.link}>
+                GitHub
+              </a>
             </button>
-          )}
+            {demo && (
+              <button className={styles.githubButton}>
+                <a href={demo} className={styles.link}>
+                  Live demo
+                </a>
+              </button>
+            )}
+            {youtube && (
+              <button className={styles.githubButton}>
+                <a href={youtube} className={styles.link}>
+                  YouTube
+                </a>
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
